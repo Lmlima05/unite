16cd549d7ccba92160b4fff592a16c8e35c96152
Luan Melo <luanmelolima@gmail.com>
2024-09-22 09:36:45 -0300

    Instalando o prisma e configurando .env

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,6 +1,11 @@
-interface Pessoa {
-  name: string
-  age: number
-}
+import fastify from "fastify";
 
-console.log('Hello World')
+const app = fastify()
+
+app.get('/', () => {
+  return 'Hello NLW Unite'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})

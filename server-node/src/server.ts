070dfdca6624bdf4e0a4c39873d68adf5cb8282b
Luan Melo <luanmelolima@gmail.com>
2024-09-24 22:02:00 -0300

    criando primeira rota

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -2,8 +2,10 @@ import fastify from "fastify";
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello NLW Unite'
+app.get('/events', (request, reply) => {
+  console.log(request.body)
+
+  return 'Hello NLW'
 })
 
 app.listen({ port: 3333 }).then(() => {

5d35b46b904c333f16183b9fbb341b8eb3d61aa2
Luan Melo <luanmelolima@gmail.com>
2024-09-25 23:50:45 -0300

    validando com zod

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,11 +1,27 @@
 import fastify from "fastify";
+import { z } from 'zod'
 
 const app = fastify()
 
-app.get('/events', (request, reply) => {
-  console.log(request.body)
+app.get('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),\
+    details: z.string().nullable(),
+    maximumattendees: z.number().int().positive().nullable(),
+  })
 
-  return 'Hello NLW'
+  const data = createEventSchema.parse(request.body)
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumattendees: data.maximumattendees,
+      slug: new Date().toISOString(),
+    },
+  })
+
+  return reply.status(201).send({ event: event.id})
 })
 
 app.listen({ port: 3333 }).then(() => {

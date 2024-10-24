c01b7806ce783d4a097a70bbccc188a5c2fd8bbb
Luan Melo <luanmelolima@gmail.com>
2024-10-24 08:49:53 -0300

    Realizando a diferenciação de erros

--- server-node/src/routes/error-handle.ts
+++ server-node/src/routes/error-handle.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 
-type FastifyErrorHandler = FastifyErrorHandler['errorHandler']
+type FastifyErrorHandler = FastifyInstance['errorHandler']
 
 export const erroHanddler: FastifyErrorHandler = (error, request, reply ) => {
   return reply.status(500).send({ message: 'Um erro aconteceu'})
